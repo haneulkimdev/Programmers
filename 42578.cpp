a4701d8df6c6a03e849a3dba710368a9dd3ca834
Haneul Kim <haneul.kim.dev@gmail.com>
2023-04-05 17:00:35 +0900

    [해시] 위장

---
+++ 42578.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+  int answer = 1;
+  unordered_map<string, int> m;
+  for (auto c : clothes) m[c[1]]++;
+  for (auto e : m) answer *= e.second + 1;
+  return answer - 1;
+}

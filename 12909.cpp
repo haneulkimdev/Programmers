01179f5a1c17150e7d33a347aab48ac88aa40955
Haneul Kim <haneul.kim.dev@gmail.com>
2023-04-14 17:21:53 +0900

    [스택/큐] 올바른 괄호

---
+++ 12909.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solution(string s) {
+  bool answer = true;
+  stack<char> st;
+  for (auto c : s) {
+    if (c == '(') {
+      st.push(c);
+    } else if (c == ')') {
+      if (st.empty() || st.top() != '(') {
+        answer = false;
+        break;
+      }
+      st.pop();
+    }
+  }
+  if (!st.empty()) answer = false;
+  return answer;
+}
